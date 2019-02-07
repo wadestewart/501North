0f65dce102a09e71a80865e434fa31edd5c7ba94
Wade Stewart <stew.wade@gmail.com>
2019-02-07 14:19:03 -0500

    post functionality working, after 12 tests lol!

--- controller/comments.js
+++ controller/comments.js
@@ -4,14 +4,16 @@ const router = express.Router()
 const Comment = require('../model/schema')
 
 router.get('/', (req, res) => {
-    Comment.find({})
-        // .then(comments => console.log(comments))
+    Comment
+        .find({})
         .then(comments => res.render('index', { comments }))
         .catch(err => console.log(err))
 })
 
-router.get('/new', (req, res) => {
-    res.render('/new')
+router.post('/', (req, res) => {
+    Comment
+        .create(req.body)
+        .then(comment => res.redirect('/'))
 })
 
 module.exports = router
