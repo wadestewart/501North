613fd6cd1ee5d6433db34f6ae9ad64526b56b595
Wade Stewart <stew.wade@gmail.com>
2019-02-06 19:21:07 -0500

    added and set up connection file, mongoose connected to mongodb

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+// In order to reference mongoose, we need to require the node module and save it to a variable
+const mongoose = require('mongoose')
+
+// Linking mongoose to our database
+mongoose.connect('mongodb://localhost/comment')
+
+// using mongoose promise library
+mongoose.Promise = Promise
+
+// When this file is required in other files, it will evaluate to this connected version of mongoose
+module.exports = mongoose
