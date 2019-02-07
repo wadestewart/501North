bbc861a7a2fa830098b458bc9ae0e362fe9350da
Wade Stewart <stew.wade@gmail.com>
2019-02-07 10:34:08 -0500

    added get route to controller, installed method-override and body-parser dependencies, set up views folder

--- controller/comments.js
+++ controller/comments.js
@@ -2,3 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const Comment = require('../model/schema')
+
+router.get('/', (req, res) => {
+    Comment.find({})
+        .then(comments => {
+            res.render('index', { comments })
+        })
+})
