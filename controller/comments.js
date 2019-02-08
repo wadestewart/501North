7163aaa32270bec0ab402984a331fb0d116d042d
Wade Stewart <stew.wade@gmail.com>
2019-02-07 22:42:44 -0500

    rendering welcome page with image

--- controller/comments.js
+++ controller/comments.js
@@ -4,31 +4,35 @@ const router = express.Router()
 const Comment = require('../model/schema')
 
 router.get('/', (req, res) => {
+    res.render('welcome')
+})
+
+router.get('/comments', (req, res) => {
     Comment
         .find({})
         .then(comments => res.render('index', { comments }))
         .catch(err => console.log(err))
 })
 
-router.post('/', (req, res) => {
+router.post('/comments', (req, res) => {
     Comment
         .create(req.body)
         .then(() => res.redirect('/'))
 })
 
-router.get('/edit/:id', (req, res) => {
+router.get('/comments/edit/:id', (req, res) => {
     Comment
         .findOne({ _id: req.params.id })
         .then(comment => res.render('edit-comment', comment))
 })
 
-router.put('/:id', (req, res) => {
+router.put('/comments/:id', (req, res) => {
     Comment
         .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
         .then(comment => res.redirect('/'))
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/comments/:id', (req, res) => {
     Comment
         .findOneAndRemove({ _id: req.params.id })
         .then(() => res.redirect('/'))
