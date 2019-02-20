c61e4afbf26eb1ed30e50106f145fa97f36cbc6c
Wade Stewart <stew.wade@gmail.com>
2019-02-20 13:47:31 -0500

    removed mongoDB files from this branch, go to mongodb branch for mongoDB crud app

--- controller/comments.js
+++ controller/comments.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db/queries')
 
-// const Comment = require('../model/schema')
+// Used this article as a guide: https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8
 
 router.get('/', (req, res) => {
     res.render('welcome')
@@ -10,44 +10,12 @@ router.get('/', (req, res) => {
 
 router.get('/comments', db.getComments)
 
-// router.get('/comments', (req, res) => {
-//     Comment
-//         .find({})
-//         .then(comments => res.render('index', { comments }))
-//         .catch(err => console.log(err))
-// })
-
 router.post('/comments', db.createComment)
-
-// router.post('/comments', (req, res) => {
-//         Comment
-//             .create(req.body)
-//             .then(() => res.redirect('/comments'))
-// })
     
 router.get('/comments/edit/:id', db.getCommentById)
-    
-// router.get('/comments/edit/:id', (req, res) => {
-//     Comment
-//         .findOne({ _id: req.params.id })
-//         .then(comment => console.log(comment))
-//         .then(comment => res.render('edit-comment', comment))
-// })
 
 router.put('/comments/:id', db.updateComment)
 
-// router.put('/comments/:id', (req, res) => {
-//     Comment
-//         .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-//         .then(() => res.redirect('/comments'))
-// })
-
 router.delete('/comments/:id', db.deleteComment)
-
-// router.delete('/comments/:id', (req, res) => {
-//     Comment
-//         .findOneAndRemove({ _id: req.params.id })
-//         .then(() => res.redirect('/comments'))
-// })
 
 module.exports = router
