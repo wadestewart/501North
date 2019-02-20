71fef842980cb1c221dfb92bbca48c57893b77b8
Wade Stewart <stew.wade@gmail.com>
2019-02-20 13:24:02 -0500

    finally targeted single comment, took 3 hours. Sheesh

--- controller/comments.js
+++ controller/comments.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db/queries')
 
-// const Comment = require('../model/schema')
+const Comment = require('../model/schema')
 
 router.get('/', (req, res) => {
     res.render('welcome')
@@ -20,24 +20,27 @@ router.get('/comments', db.getComments)
 router.post('/comments', db.createComment)
 
 // router.post('/comments', (req, res) => {
-    //     Comment
-    //         .create(req.body)
-    //         .then(() => res.redirect('/comments'))
-    // })
+//         Comment
+//             .create(req.body)
+//             .then(() => res.redirect('/comments'))
+// })
     
 router.get('/comments/edit/:id', db.getCommentById)
     
 // router.get('/comments/edit/:id', (req, res) => {
 //     Comment
 //         .findOne({ _id: req.params.id })
+//         .then(comment => console.log(comment))
 //         .then(comment => res.render('edit-comment', comment))
 // })
 
-router.put('/comments/:id', (req, res) => {
-    Comment
-        .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-        .then(() => res.redirect('/comments'))
-})
+router.put('/comments/:id', db.updateComment)
+
+// router.put('/comments/:id', (req, res) => {
+//     Comment
+//         .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+//         .then(() => res.redirect('/comments'))
+// })
 
 router.delete('/comments/:id', db.deleteComment)
 
@@ -45,6 +48,6 @@ router.delete('/comments/:id', db.deleteComment)
 //     Comment
 //         .findOneAndRemove({ _id: req.params.id })
 //         .then(() => res.redirect('/comments'))
-//   })
+// })
 
 module.exports = router
--- db/queries.js
+++ db/queries.js
@@ -8,7 +8,8 @@ const pool = new Pool({
 })
 
 const getComments = (req, res) => {
-    pool.query('SELECT * FROM comments ORDER BY id ASC', (err, results) => {
+    pool
+        .query('SELECT * FROM comments ORDER BY id ASC', (err, results) => {
         let comments = results.rows
         if (err) {
             throw err
@@ -24,41 +25,37 @@ const createComment = (req, res) => {
 
     pool
         .query('INSERT INTO comments (user_comment) VALUES ($1)', [user_comment])
-        // .then(() => console.log(req.body.user_comment))
         .then(() => res.redirect('/comments'))
         .catch(e => setImmediate(() => { throw e }))
-
-    // pool
-    //     .query('INSERT INTO comments (user_comment) VALUES ($1)', [user_comment], (err, result) => {
-    //     if (err) {
-    //         throw err
-    //     }
-    //     res
-    //         .status(201)
-    //         // .send(`Comment added with ID: ${result.insertId}`)
-    //         .redirect('/comments', () => console.log(`Comment added with ID: ${result.insertId}`))
-    // })
 }
 
 const getCommentById = (req, res) => {
     const id = parseInt(req.params.id)
 
-    pool.query('SELECT * FROM comments WHERE id = $1', [id], (err, result) => {
-        let comment = result.rows
-        if(err) {
-            throw err
-        }
-        res
-            .status(200)
-            .render('edit-comment', comment)
-    })
+    pool
+        // .query('SELECT * FROM comments WHERE id = $1', [id])
+        // .then(comment => console.log(comment.rows))
+        // .then(comment => res.render('edit-comment', comment))
+        // .catch(e => setImmediate(() => { throw e }))
+
+        .query('SELECT * FROM comments WHERE id = $1', [id], (err, result) => {
+            const comment = result.rows[0]
+            console.log(comment)
+            if (err) {
+                throw err
+            }
+            res
+                .status(200)
+                .render('edit-comment', comment)
+        })
+            
 }
 
 const updateComment = (req, res) => {
     const id = parseInt(req.params.id)
     const user_comment = req.body
 
-    pool.query('UPDATE comments SET user_comment = $1 WHERE id = $2', [user_comment], (err, results) =>{
+    pool.query('UPDATE comments SET user_comment = $1 WHERE id = $2', [user_comment, id], (err, results) =>{
         if (err) {
             throw err
         }
